8e870e1b9de59f9c20257477990606e376b064af
yeongsinkeem <dudtls0522@sookmyung.ac.kr>
2025-03-28 17:02:25 +0900

    Initial commit

---
+++ src/main/java/org/example/tackit/TackitApplication.java
@@ -0,0 +1,13 @@
+package org.example.tackit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TackitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TackitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/tackit/TackitApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tackit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TackitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
