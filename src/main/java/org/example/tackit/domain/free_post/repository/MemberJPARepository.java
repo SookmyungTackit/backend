8ea593a9628537a544fff7f4b77217d273ee32c6
yeongsinkeem <dudtls0522@sookmyung.ac.kr>
2025-04-11 18:47:14 +0900

    refactor: User 테이블 이름 변경 -> Member 테이블

--- src/main/java/org/example/tackit/domain/free_post/repository/MemberJPARepository.java
+++ src/main/java/org/example/tackit/domain/free_post/repository/MemberJPARepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserJPARepository extends JpaRepository<Member, Integer> {
+public interface MemberJPARepository extends JpaRepository<Member, Integer> {
     Member findTopByOrderByIdAsc();
     Optional<Member> findByNickname(String nickname);
 }
