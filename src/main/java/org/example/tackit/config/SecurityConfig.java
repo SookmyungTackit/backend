718ca78cd3279c0c470165d4149f00cc2769dd44
semsemin <semin0409@naver.com>
2025-05-16 03:44:39 +0900

    feat:#38-security cors 설정

--- src/main/java/org/example/tackit/config/SecurityConfig.java
+++ src/main/java/org/example/tackit/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package org.example.tackit.config;
 import lombok.RequiredArgsConstructor;
 import org.example.tackit.config.jwt.JwtAccessDeniedHandler;
 import org.example.tackit.config.jwt.JwtAuthenticationEntryPoint;
+import org.example.tackit.config.jwt.JwtFilter;
 import org.example.tackit.config.jwt.TokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,25 +15,49 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JwtFilter jwtFilter;
     private final TokenProvider tokenProvider;
-    private final CorsFilter corsFilter;
     private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+    // cors 설정
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(Arrays.asList(
+                "http://localhost:3000", "https://test-cors.org"
+        ));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
                 .csrf(csrf -> csrf.disable())
+                .formLogin(formLogin -> formLogin.disable()) //form login 비활성화
+                .httpBasic(httpBasic -> httpBasic.disable()) // http 기본인증 비활성화
+                .cors(corsConfigurer -> corsConfigurer.configurationSource(corsConfigurationSource()))
 
-                .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
                 .exceptionHandling(exception -> {
                     exception.accessDeniedHandler(jwtAccessDeniedHandler);
                     exception.authenticationEntryPoint(jwtAuthenticationEntryPoint);
@@ -48,6 +73,7 @@ public class SecurityConfig {
                         .permitAll()
                         .anyRequest().authenticated()
                 )
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 // JwtFilter 를 addFilterBefore 로 등록했던 JwtSecurityConfig 클래스를 적용
                 .with(new JwtSecurityConfig(tokenProvider), customizer -> customizer.getClass());
 
