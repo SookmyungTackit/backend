75bd2a86f12617f472348866f6e85d29f921fe76
semsemin <semin0409@naver.com>
2025-05-26 00:49:49 +0900

    feat:#49-게시글 전체 조회 페이징

--- src/main/java/org/example/tackit/domain/QnA_board/QnA_post/repository/QnAPostRepository.java
+++ src/main/java/org/example/tackit/domain/QnA_board/QnA_post/repository/QnAPostRepository.java
@@ -4,6 +4,8 @@ import org.example.tackit.domain.entity.Member;
 import org.example.tackit.domain.entity.Post;
 import org.example.tackit.domain.entity.QnAPost;
 import org.example.tackit.domain.entity.Status;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -14,6 +16,6 @@ public interface QnAPostRepository extends JpaRepository<QnAPost, Long> {
     // 선택적으로 질문 게시글만 조회할 때 사용
     List<QnAPost> findByType(Post type);
     // 상태로 조회
-    List<QnAPost> findAllByStatusAndWriter_Organization(Status status, String organization);
+    Page<QnAPost> findAllByStatusAndWriter_Organization(Status status, String org, Pageable pageable);
     List<QnAPost> findByWriter(Member member);
 }
