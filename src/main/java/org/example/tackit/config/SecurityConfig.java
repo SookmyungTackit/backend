9e205664cad27f6b8985d06fb8b2e344a0e87158
semsemin <semin0409@naver.com>
2025-06-09 16:37:42 +0900

    feat:#83-cors 추가

--- src/main/java/org/example/tackit/config/SecurityConfig.java
+++ src/main/java/org/example/tackit/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOriginPatterns(Arrays.asList(
-                "http://localhost:3000", "http://localhost:3005"
+                "http://localhost:3005", "https://tackit-frontend.vercel.app", "https://tackit-frontend.vercel.app/"
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
