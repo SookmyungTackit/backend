82f15517e7f810cdcc03dbe979b9b830510e4eaa
semsemin <semin0409@naver.com>
2025-09-19 04:21:01 +0900

    fix:#109 - 1년마다 역할 자동 갱신

--- src/main/java/org/example/tackit/TackitApplication.java
+++ src/main/java/org/example/tackit/TackitApplication.java
@@ -2,10 +2,12 @@ package org.example.tackit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.TimeZone;
 
 @SpringBootApplication
+@EnableScheduling
 public class TackitApplication {
 
     public static void main(String[] args) {
