bbebd2e05b8308d4a4c641af1c1b988935d6cd40
yeongsinkeem <dudtls0522@sookmyung.ac.kr>
2025-09-26 14:21:00 +0900

    refactor: 스크립트 파일 주석 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,5 +3,5 @@
 # 기존에 실행되던 앱 종료
 pkill -f '.*\.jar'
 
-# 새 .jar 파일 실행 (로그는 nohup.out에 기록됨)
+# 새 .jar 파일 실행 (로그는 nohup.out에 기록)
 nohup /usr/bin/java -jar /home/ubuntu/app/*.jar > /home/ubuntu/app/nohup.out 2>&1 &
